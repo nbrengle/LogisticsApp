63c62387261b2f0510cb861f1ff5c4b6864bf66c
fyreswordgames <fyreswordgames@gmail.com>
2016-05-30 20:37:20 -0700

    getters and setters for Order

--- LogisticsApp/src/order/impl/OrderRegImpl.java
+++ LogisticsApp/src/order/impl/OrderRegImpl.java
@@ -1,5 +1,6 @@
 package order.impl;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 
 import order.interfaces.Order;
@@ -11,6 +12,7 @@ public class OrderRegImpl implements Order {
 	private int startDay;
 	private String destinationCity;
 	private String destinationState;
+	private String destinationUniqueIdentifier;
 	private HashMap<String, Integer> items = null;
 	
 	public OrderRegImpl(String idIn, 
@@ -22,15 +24,22 @@ public class OrderRegImpl implements Order {
 		setStartDay(startDayIn);
 		setDestinationCity(destCityIn);
 		setDestinationState(destStateIn);
+		setDestinationUniqueIdentifier(destCityIn, destStateIn);
 		setItems(itemsIn);
 		
+	}
+
+	private void validateStringInput(String input) {
+		if (input.equals(null)) { throw new InvalidParameterException("An Order's " + input + " cannot be null"); }
+		if (input.equals("")) { throw new NullPointerException("An Order's " + input + " cannot be empty"); }
 	}
 
 	public String getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	private void setId(String id) {
+		validateStringInput(id);
 		this.id = id;
 	}
 
@@ -38,7 +47,8 @@ public class OrderRegImpl implements Order {
 		return startDay;
 	}
 
-	public void setStartDay(int startDay) {
+	private void setStartDay(int startDay) {
+		if (startDay <= 0) { throw new InvalidParameterException("startDay must be greater than 0"); }
 		this.startDay = startDay;
 	}
 
@@ -46,7 +56,8 @@ public class OrderRegImpl implements Order {
 		return destinationCity;
 	}
 
-	public void setDestinationCity(String destinationCity) {
+	private void setDestinationCity(String destinationCity) {
+		validateStringInput(destinationCity);
 		this.destinationCity = destinationCity;
 	}
 
@@ -54,15 +65,29 @@ public class OrderRegImpl implements Order {
 		return destinationState;
 	}
 
-	public void setDestinationState(String destinationState) {
+	private void setDestinationState(String destinationState) {
+		validateStringInput(destinationState);
 		this.destinationState = destinationState;
 	}
 
 	public HashMap<String, Integer> getItems() {
+		//TODO THIS IS ALL WRONG HUGE INFORMATION HIDING VIOLATON
 		return items;
 	}
 
-	public void setItems(HashMap<String, Integer> items) {
+	private void setItems(HashMap<String, Integer> items) {
+		if (items.isEmpty()) {throw new InvalidParameterException("An Order must contain items"); }
+		if (items.equals(null)) {throw new NullPointerException("An Order cannot have null items"); }
 		this.items = items;
+	}
+	
+	public String getDestinationUniqueIdentifier() {
+		return destinationUniqueIdentifier;
+	}
+	
+	private void setDestinationUniqueIdentifier(String destinationCity, String destinationState) {
+		validateStringInput(destinationCity); 
+		validateStringInput(destinationState); 
+		this.destinationUniqueIdentifier = String.format("%s, %s", destinationCity, destinationState);
 	}
 }
